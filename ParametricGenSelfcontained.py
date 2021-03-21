fce8efa3ec94d74f59245d8894b8ae7423b3ddb8
Peter <peterbowmandavis21@js-student.org>
2021-03-20 23:12:08 -0400

    Cleaned up Metrics (now called SequenceMetricGeneration.py), continued to refactor main file.

--- ParametricGenSelfcontained.py
+++ ParametricGenSelfcontained.py
@@ -31,6 +31,7 @@ import shutil
 import subprocess
 import time
 from multiprocessing import Pool
+import SequenceMetricGeneration
 
 millis = int(round(time.time() * 1000))
 
@@ -66,13 +67,13 @@ def get_moststable(sequence):  # RNALfold Stable Structure Finder
 
 
 def gen_diversity():
-    with open('diversity.txt') as j:
+    with open('gkm_svm_output.txt') as j:
         line_count = 0
         for line in j:
             line_count += 1
     j.close()
     diversityscores = []
-    f = open("diversity.txt", "r")
+    f = open("gkm_svm_output.txt", "r")
     for z in range(0, line_count):
         temp = f.readline()
         if temp != "" and temp != "\n":
@@ -221,40 +222,55 @@ def length_based_generation(length):
     # for library diversity given limited computational power. if all sequences are generated then this is not needed
     random.shuffle(InverseReady)
 
+
 # assembles the individually generated txt files into a unified text file
 def assemble():
-    namelist = os.listdir("/Users/Peter/Desktop/RNALibrary/AssemblySeq/")
+    namelist = os.listdir(project_path+'AssemblySeq/')
 
     for name in namelist:
-        print(name)
         with open(name) as f:
-            with open("testrun.txt", "a") as f1:
+            with open('InverseSequenceOutput.txt', 'a') as f1:
                 for line in f:
                     f1.write(line)
-    f1.close()
-
-    shutil.copyfile('/Users/Peter/Desktop/RNALibrary/AssemblySeq/testrun.txt',
-                    '/Users/Peter/Desktop/RNALibrary/testrun.txt')
-    os.chdir("/Users/Peter/Desktop/RNALibrary/")
-    shutil.rmtree('AssemblySeq')
-
-
-if __name__ == '__main__':
-
-    open('testrun.txt', 'w').close()
-    os.chdir("/Users/Peter/Desktop/RNALibrary/")
     try:
-        shutil.rmtree('AssemblySeq')
+        f1.close()
     except:
         pass
-    os.mkdir("/Users/Peter/Desktop/RNALibrary/AssemblySeq/")
-    os.chdir("/Users/Peter/Desktop/RNALibrary/AssemblySeq/")
 
-    length_based_generation(25)
-    fullresource_inverse(InverseReady[0:10])
+    shutil.copyfile(project_path+'AssemblySeq/InverseSequenceOutput.txt',
+                    project_path+'InverseSequenceOutput.txt')
+    os.chdir(project_path)
+    shutil.rmtree('AssemblySeq')
+
+# specifies the path of where you want to run this script. Note that if you are running
+# the R script or Metrics, those files have to be in the same directory
+project_path = '/Users/Peter/PycharmProjects/RNALibrary/'
+
+if __name__ == '__main__':
+    # all of this just clears the files for a re-run
+
+    sequence_count=2
+    sequence_length=120
+
+    os.chdir(project_path)
+    open('InverseSequenceOutput.txt', 'w').close()
+    os.chdir(project_path)
+
+    try:
+        os.mkdir(project_path+'AssemblySeq/')
+    except:
+        shutil.rmtree('AssemblySeq')
+        os.mkdir(project_path + 'AssemblySeq/')
+
+    os.chdir(project_path+'AssemblySeq/')
+
+    # generation and inverse of the sequences
+    length_based_generation(sequence_length)
+    fullresource_inverse(InverseReady[0:sequence_count])
     assemble()
 
-    # subprocess.call ("Rscript TestPipeline1.R", shell=True)
-    # diversityscores=GenDiversity()
-    # NewMetrics.RetrieveFile(100)
-    # NewMetrics.GenMetricFile(diversityscores)
+    subprocess.call ("Rscript gkm_svm_generator.R", shell=True)
+    gkm_svm_scores=gen_diversity()
+
+    SequenceMetricGeneration.RetrieveFile(sequence_count)
+    SequenceMetricGeneration.GenMetricFile(gkm_svm_scores)
--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # RNA Library
-A project oriented towards the exploration of Non-Coding Regions within RNA, to study specific folding structures and their effect upon the production of proteins.
+Generates a large amount of mRNA sequences via ViennaRNA's inverse folding and generates metrics for later physical synthesis and exploration of mRNA's secondary structure.
+
 ## Requisite Software
+The most current code for this project utilyzes Python 3.8 and R 3.6.3 syntax 
+
 We use the ViennaRNA Package which can be found [here](https://www.tbi.univie.ac.at/RNA/) for folding and inverse folding functionality
+
+The external R package, 'gkmSVM' is also a requisite for certain metric generation functionality, which may be found [here](http://www.beerlab.org/gkmsvm/)
+
 ## Authors
-Peter Bowman-Davis, under the guidance of Dr. Stephen Floor
+Peter Bowman-Davis, under the guidance of Stephen Floor, Ph.D, and Yizhu Lin, Ph.D 
